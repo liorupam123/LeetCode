78bca95c78980a6349d4b411b34db9a2ec550add
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2021-12-23 22:44:55 +0530

    Time: 116 ms (18.26%), Space: 44.4 MB (7.70%) - LeetHub

---
+++ subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+	    // Optimal Solution Time O(N) & Auxiliary Space O(N)
+        int ls=0; 
+        int len=nums.size(), count=0; 
+        map<int,int> m;
+        m[0]=1; 
+		             
+					 
+        for(int i=0;i<len;i++){
+            ls+=nums[i];
+            count+=m[ls-k];
+            m[ls]++;
+        }
+        return count;
+    }
+};
