72c2b26263a0b93d279dd0bf3d79094e31e7d1cb
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2021-12-27 20:19:58 +0530

    Time: 4 ms (99.59%), Space: 15.8 MB (48.17%) - LeetHub

--- majority-element-ii/majority-element-ii.cpp
+++ majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
+        int sz = nums.size();
+        int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
+        for (i = 0; i < sz; i++){
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
+            else if (count1 == 0){
+                num1 = nums[i];
+                count1 = 1;
+            }    
+            else if (count2 == 0){
+                num2 = nums[i];
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
         vector<int> ans;
-        // storing the element and its count
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-          mp[nums[i]]++;
+        count1 = count2 = 0;
+        for (i = 0; i < sz; i++){
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
         }
-        for(auto it:mp){
-          
-          // if any elements's count is > n/2
-          if(it.second>n/3){
-            // return the element
-            ans.push_back(it.first);
-          }
-        }
+        if (count1 > sz/3)
+            ans.push_back(num1);
+        if (count2 > sz/3)
+            ans.push_back(num2);
         return ans;
     }
 };
