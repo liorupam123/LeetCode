e7f48de8d86e1f4050b40910461e3f24783ad780
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-06 10:17:11 +0530

    Time: 11 ms (25.14%), Space: 7.5 MB (83.43%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -7,21 +7,24 @@ public:
         int max_len=0;
         
         while(high<len){
+            // calculating the cost at first
             currCost+=abs(s[high]-t[high]);
             
+            // if cost > maxcost , then delete from first and move the window
             if(currCost>maxCost){
               while(currCost>maxCost){
                 currCost-=abs(s[low]-t[low]);
                 low++;
               }
             }
+            // else , when cost<= maxcost , calculate the maximum window size
+            // it is said in question then check when cost less than or equal to max cost
             else{
               max_len=max(max_len,high-low+1);
             }
+            // high/j++ always to get the window/move the window
             high++;
               
-              
-            
         }
         return max_len;
     }
