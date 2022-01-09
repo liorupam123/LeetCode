b6399b442433a1ae8c160bb04324b08a724ad20f
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-09 20:32:02 +0530

    Time: 60 ms (50.36%), Space: 55.4 MB (62.30%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -3,7 +3,7 @@ public:
     int longestOnes(vector<int>& nums, int k) {
         int start = 0, end = 0, zeros = 0, res = 0;
         
-        for (end = 0; end < nums.size(); end++) {
+        while(end<nums.size()){
             
             // If we reached a zero, increase the zero counter.
             if (nums[end]==0)
@@ -11,12 +11,15 @@ public:
             
             // If the number of zeros is more than k, we finished with this subarray.
             // Now we can save the length and increament start to find new size
-            while (zeros > k) {
+            if(zeros>k){
+              while (zeros > k) {
                 if (nums[start]==0) zeros--;
                 start++;
-            } 
+              } 
+            }
             
             res = max(res, end - start + 1);
+            end++;
         }
         
         return res;
