000261d9444e5f80cf2782ea0f38876e97436c2f
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-10 17:03:58 +0530

    Time: 3 ms (82.04%), Space: 6.8 MB (40.98%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+      
+        // unordered map of char and its last index
+        unordered_map<char,int> map;
+        vector<int> ans;
+        if(S.size()==0)return ans;
+        // updating the last index of charater
+        for(int i=0;i<S.size();i++)
+            map[S[i]]=i;
+      
+        // taking the maxindex = 0 and lastindex = 1
+        int maxindex=0,lastindex=-1;
+        for(int i=0;i<S.size();i++){
+          
+            // now iterate through the array and check the maxindex of the characters
+            maxindex=max(maxindex,map[S[i]]);
+            // when i reached the maxindex of one charater
+            if(i==maxindex){
+                // push the length in the answer
+                ans.push_back(maxindex-lastindex);
+                // update the lastindex with the maxindex
+                lastindex=maxindex;
+            }
+        }
+        return ans;
+    }
+};
