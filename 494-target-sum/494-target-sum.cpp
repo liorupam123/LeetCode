a3b7aca8d44e2a1f9743f00a1e04492137f7f760
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-17 19:02:44 +0530

    Time: 1340 ms (9.72%), Space: 9.1 MB (74.50%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-      int targetSum(vector<int>& nums,int currentIndx, int target,unordered_map<string,int>&mp){
-            if(target == 0 &&  currentIndx==nums.size() )
+    
+     int  targetSum(vector<int>& nums,int currentIndx, int target)
+     {
+               if( currentIndx >= nums.size() &&target != 0)
+            return 0;
+        
+      
+             if(target == 0 &&  currentIndx>=nums.size() )
             return 1;
         
-            if( target!=0 && currentIndx >= nums.size())
-            return 0;
-            
-            string currentkey = to_string(currentIndx) + "_" + to_string(target);
-            if(mp.find(currentkey)!=mp.end()){
-              return mp[currentkey];
-            }
-            int pick = targetSum(nums,currentIndx+ 1, target - nums[currentIndx],mp);
-            int nonpick=targetSum(nums,currentIndx + 1, target + nums[currentIndx],mp);
+        int pick = targetSum(nums,  currentIndx+ 1, target - nums[ currentIndx]);
+              int nonpick=targetSum(nums, currentIndx + 1, target + nums[ currentIndx]);
          
-            mp[currentkey] = pick+nonpick;
-            return mp[currentkey];
+          return pick+nonpick;
      }
+    
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string,int> mp;
-        return targetSum(nums,0,target,mp);
+      
+        return targetSum(nums, 0, target);
     }
 };
+
+
+
+
