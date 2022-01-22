73dda3b462fa29eed1d25661cb435294f21d5c6b
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-22 12:36:43 +0530

    Added solution - LeetHub

--- Plus One - GFG/plus-one.cpp
+++ Plus One - GFG/plus-one.cpp
@@ -10,18 +10,23 @@ class Solution {
   public:
     vector<int> increment(vector<int> arr ,int N) {
         // code here
-        vector<int>v;
+            vector<int>v;
+            // at first carry = 1, because we have to add 1 at the end
             int carry = 1;
             for(int i = N-1; i>=0; --i){
                 int pu = (arr[i]+carry)%10;
                 v.push_back(pu);
+                // after that if the arr[i]!=9, we don't have to carry anything
                 if(arr[i]!=9){
                     carry=0;
                 }
             }
+            // if the first element of the array == 9, it means 9+1 =10, so we have to add 1
+            // at the end of the array
             if(carry==1 && arr[0]==9){
                 v.push_back(1);
             }
+            // reverse the answer array
             reverse(v.begin(), v.end());
             return v;
     }
