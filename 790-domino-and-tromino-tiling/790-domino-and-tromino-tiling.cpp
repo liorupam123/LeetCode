2d19c7ac198c8b103123b71de217f1c3f77e32d5
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-29 17:23:01 +0530

    Time: 6 ms (12.87%), Space: 5.9 MB (74.73%) - LeetHub

---
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/domino-and-tromino-tiling/discuss/1620975/C%2B%2BPython-Simple-Solution-w-Images-and-Explanation-or-Optimization-from-Brute-Force-to-DP
+
+class Solution {
+public:
+    const int MOD = 1e9+7;
+    int dp[1001][2]{};
+    int numTilings(int n) {
+        return solve(0, n, false);
+    }
+    long solve(int i, int n, bool previousGap) {
+        if(i > n) return 0;
+        if(i == n) return !previousGap;
+        if(dp[i][previousGap]) return dp[i][previousGap];
+        if(previousGap)
+            return dp[i][previousGap] = (solve(i+1, n, false) + solve(i+1, n, true)) % MOD;
+        return dp[i][previousGap] = (solve(i+1, n, false) + solve(i+2, n, false) + 2*solve(i+2, n, true)) % MOD;
+    }
+};
