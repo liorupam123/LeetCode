1f45484e64e4749d300475a20fa493d59314bd81
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-02-26 13:13:00 +0530

    Time: 0 ms (100.00%), Space: 16.4 MB (74.99%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -9,45 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    bool solve(TreeNode * r1, TreeNode * r2){
-        // See the tree diagram are r1 and r2 null ? No, so this line dont execute
-        if(r1 == NULL && r2 == NULL)
-            return true; 
-		
-        // Is any one of r1 or r2 null ? Or are these values different ? No. Both values are
-        // same so this else if wont execute either
-        else if(r1 == NULL || r2 == NULL )
-            return false; 
-        
-        // Now comes the main part, we are calling 2 seperate function calls 
-        return (r1->val == r2->val) && solve(r1->left, r2->right) && solve(r1->right, r2->left);
-        // First solve() before && will execute
-        // r1->left is 3 and r2->right = 3
-        // Both values are same , they will by pass both if and else if statement
-        // Now again r1->left is null and r2->right is null
-        // So they will return true from first if condtion
-        // Now the scene is : we have executed first solve() before && and it has
-        // returned us True so expression becomes ' return true && solve() '
-        // Now solve after && will execute 
-        // Similarly it will check for 4 and 4 , it will by pass if else statements
-        // next time both will become null, so will return true
-        // Thus 2nd solve() at the end will also hold true
-        // and we know 'true && true' is true
-        // so true will be returned to caller, and thus tree is mirror of itself.
-        // Similarly you can check for any testcase, flow of execution will remain same.
-        
-    }
-    
-    bool isSymmetric(TreeNode* root) 
-    {
-        // Imagine a tree:    1
-        //                 2     2
-        //               3   4 4   3
-        // We are standing on root that is 1, function begins
-        // and now r1 and r2 points to 2 and 2 respectively.                      
-        return solve(root->left, root->right);     
+    bool isSymmetric(TreeNode* root) {
+      TreeNode* p = root->left;
+      TreeNode* q = root->right;
+         queue<TreeNode*> queue;
+        if(p== NULL && q==NULL) return true;
+        if(p== NULL || q==NULL) return false;
+      
+        queue.push(p); queue.push(q);
+        while(!queue.empty())  {
+            TreeNode* left = queue.front(); queue.pop();
+            TreeNode *right = queue.front(); queue.pop();
+            if(left==NULL && right==NULL) continue;
+            if(left==NULL || right==NULL) return false;
+            if(left->val != right->val) return false;
+            queue.push(left->left);
+            queue.push(right->right);
+          
+            queue.push(left->right);
+            queue.push(right->left);
+            
+            
+        }
+        return true;  
     }
 };
