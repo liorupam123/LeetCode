3c1d3d3844521cb10a6ccb4f32ccf3de70ef189d
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-03-16 17:34:18 +0530

    Time: 8 ms (86.39%), Space: 14.2 MB (38.07%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    void floodFillRecursive(vector<vector<int>>& image, int sr,int sc,int n,int m,int source,int newColor){
+    void floodFillBFS(vector<vector<int>>& image, int sr,int sc,int n,int m,int source,int newColor){
       
         
-        if(sr<0 || sr>=n || sc>=m || sc<0 || image[sr][sc]!=source) return;
-      
-        image[sr][sc] = newColor;
-      
-        floodFillRecursive(image,sr+1,sc,n,m,source,newColor);
-        floodFillRecursive(image,sr-1,sc,n,m,source,newColor);
-        floodFillRecursive(image,sr,sc+1,n,m,source,newColor);
-        floodFillRecursive(image,sr,sc-1,n,m,source,newColor);
-      
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int curr_row = it.first;
+            int curr_col = it.second;
+            if(curr_row < 0 || curr_row>=n ||curr_col < 0 || curr_col >=m || image[curr_row][curr_col] != source ){
+            continue;
+            }
+            
+             // like number of island, change the source to new color
+            image[curr_row][curr_col] = newColor;
+          
+            q.push({curr_row-1,curr_col});
+            q.push({curr_row,curr_col+1});
+            q.push({curr_row+1,curr_col});
+            q.push({curr_row,curr_col-1});
+        }
+        
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         
@@ -21,7 +33,7 @@ public:
         int m = image[0].size();
         int source = image[sr][sc];
         
-        floodFillRecursive(image,sr,sc,n,m,source,newColor);
+        floodFillBFS(image,sr,sc,n,m,source,newColor);
         return image;
     }
 };
