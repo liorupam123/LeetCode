109e8f7d0bbd831d7241953773e20b00a19b7899
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-03-22 19:17:37 +0530

    Time: 467 ms (41.81%), Space: 91.2 MB (51.96%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -6,6 +6,7 @@ public:
             return;
         }
         
+        // if the element of the two grid is not same, make the flag = 0
         if(grid1[i][j]!=grid2[i][j]){
             flag = 0;
         } 
@@ -29,8 +30,10 @@ public:
                 
                 if(grid2[i][j]==1){
                     
+                    // keep a bool flag = 1,
                     int flag=1;
                     dfs(i,j,grid1,grid2,flag);
+                    // after calling the function, if the flag is still true, then ans+=1
                     if(flag){
                        ans+=1;
                     }
