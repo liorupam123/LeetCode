699d9cd2c3aafd226057432a8505069cd35fa296
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-03-24 23:23:05 +0530

    Added solution - LeetHub

---
+++ Swap Kth nodes from ends - GFG/swap-kth-nodes-from-ends.cpp
@@ -0,0 +1,109 @@
+// { Driver Code Starts
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+Node *swapkthnode(Node* head, int num, int K);
+
+void addressstore(Node **arr, Node* head)
+{
+    Node* temp = head;
+    int i = 0;
+    while(temp){
+        arr[i] = temp;
+        i++;
+        temp = temp->next;
+    }
+}
+
+bool check(Node ** before, Node **after, int num, int K)
+{
+    if(K > num)
+        return 1;
+    return (before[K-1] == after[num - K]) && (before[num-K] == after[K-1]);
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int num, K , firstdata;
+        cin>>num>>K;
+        int temp;
+        cin>>firstdata;
+        Node* head = new Node(firstdata);
+        Node* tail = head;
+        for(int i = 0; i<num - 1; i++){
+            cin>>temp;
+            tail->next = new Node(temp);
+            tail = tail->next;
+        }
+        
+        Node *before[num];
+        addressstore(before, head);
+        
+        head = swapkthnode(head, num, K);
+        
+        Node *after[num];
+        addressstore(after, head);
+        
+        if(check(before, after, num, K))
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/* Linked List Node structure
+   struct Node  {
+     int data;
+     Node *next;
+     Node(int x) {
+        data = x;
+        next = NULL;
+  }
+  }
+*/
+
+//Function to swap Kth node from beginning and end in a linked list.
+Node *swapkthnode(Node* head, int n, int k)
+{
+    // Your Code here
+    Node *prev1, *node1, *node2, *copy = new Node(-1);
+  prev1 = node1 = node2  = copy;
+  copy->next = head;
+  if (k > n) {return copy->next;}
+  int ct = 1, fromLast = n - k + 1;
+  //take some test cases then you can observe that kth node from last is this
+  
+  while (head) {
+    prev1 = head;
+    head = head->next;
+    ct++;
+    if (ct == k) {
+      node1 = prev1;
+    }
+    else if (ct == fromLast) {
+      node2 = prev1;
+    }
+  }
+  swap(node1->next, node2->next);
+  swap(node1->next->next, node2->next->next);
+  return copy->next;
+}
