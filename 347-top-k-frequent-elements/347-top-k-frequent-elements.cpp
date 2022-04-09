6d2352508aca0182d5ac9e3b19ae908256800537
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-04-09 23:44:44 +0530

    Time: 27 ms (31.88%), Space: 13.7 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,36 +1,47 @@
-// Hash Map Soln
-// Time O(n.logn)
+// Heaps Soln
+// Time O(n.logk)
 // Space O(n)
+
+// comparator function for the priority queue
+class CompareFunc{
+public:
+    bool operator() ( pair<int,int> n1, pair<int,int> n2 ) {
+        return n1.second > n2.second;
+    }
+};
 
 class Solution{
 public:
     vector<int> topKFrequent(vector<int>& nums, int k){
         int n = nums.size();
-      
         // taking an unordered_map to store the frequency
         // element and frequency
         unordered_map<int, int> mymap;
         for(int i=0;i<n;i++){
           mymap[nums[i]]++;
         }
-        // vector of pair of element & frequency
-        vector<pair<int,int>> p;
-        
-        // at first store the frequency then element as a pair in the vector
+        // top k frequent -- kinda largest, so take a min heap
+        // compare function to sort according to frequency
+        priority_queue<pair<int,int>, vector<pair<int,int>>, CompareFunc> pq;
+      
+        // iterate through the map, and push the element and frequnency as a pair into the heap
         for ( auto i : mymap ) {
-            p.push_back(make_pair(i.second, i.first));
+            pq.push(make_pair(i.first, i.second));
+            // when heap size is > k ,pop the top element
+            if ( pq.size() > k ) {
+                pq.pop();
+            }
         }
-        // sort the vector in decreasing order
-        // it is sorted according to frequncy number
-        sort(p.begin(), p.end(), greater<>());
-        
         
         vector<int> res;
-        // now iterate through the vector, and push the element (p.second) in the answer vector
-        for ( int i=0; i<k; i++ ) {
-            res.push_back(p[i].second);
+      
+        // while heap is not empty, push the pq.top.first (element) in the answer vector
+        while ( !pq.empty() ) {
+            res.push_back(pq.top().first);
+            pq.pop();
         }
-        // return the answer vector at the last
+        
+        // return the answer vector at last
         return res;
     }
 };
