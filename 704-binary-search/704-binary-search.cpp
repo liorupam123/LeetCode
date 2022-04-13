fe63cfa4133eb4e1098901d18f563a215211d353
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-04-14 00:07:47 +0530

    Time: 43 ms (70.57%), Space: 27.6 MB (12.69%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int recursiveBS(vector<int>& nums,int left,int right, int target){
-      
-      while(left<=right){
-        int mid =left+(right-left)/2;
-      
-        if(nums[mid]==target){ 
-            return mid;
-        }
-        else if(nums[mid]>target){ 
-            return recursiveBS(nums,left,mid-1,target);
-        }
-        else{ 
-            return recursiveBS(nums,mid+1,right,target);
-        }
-      }
-      return -1;  
-    }
     int search(vector<int>& nums, int target) {
-      int n = nums.size();
-      return recursiveBS(nums,0,n-1,target);
-      
+        int n  = nums.size();
+        int s = 0, e = n - 1;
+        while(s<=e){
+          
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target)  e = mid - 1;
+            else  s = mid + 1;
+        }
+        return -1;
     }
-  
 };
